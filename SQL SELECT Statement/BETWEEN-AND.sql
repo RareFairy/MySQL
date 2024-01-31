dac5754521981367da8fd233079e25f1815338fd
RareFairy <85610641+RareFairy@users.noreply.github.com>
2024-01-31 13:20:01 +0200

    BETWEEN... AND...

---
+++ SQL SELECT Statement/BETWEEN-AND.sql
@@ -0,0 +1,11 @@
+select * 
+from salaries
+where salary between 66000 and 70000;
+
+select * 
+from salaries
+where emp_no between '10004' and '10012';
+
+select *
+from departments
+where dept_no between 'd003' and 'd006';
